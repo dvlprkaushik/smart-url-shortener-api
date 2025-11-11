b79bb5ee263c853fb13bca807a5c50abbef1d4bb
kaushik <dvlprkaushik@gmail.com>
2025-11-11 19:46:03 +0530

    refactor: clean code structure, improve error handling, and finalize production setup

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
-import express, { Request, Response } from "express";
+import express, { NextFunction, Request, Response } from "express";
 import dotenv from "dotenv";
 import pkg from "../package.json";
 import { listener } from "./listener.js";
+import { UrlError } from "./utils/UrlError";
 
 dotenv.config({ quiet: true });
 
@@ -11,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(express.json());
 
-// ✅ Health check route
+// Health check route
 app.get("/", (_req: Request, res: Response) => {
   res.status(200).json({
     status: "OK",
@@ -19,7 +20,7 @@ app.get("/", (_req: Request, res: Response) => {
   });
 });
 
-// ✅ Info route from package.json
+// Info route from package.json
 app.get("/info", (_req: Request, res: Response) => {
   res.status(200).json({
     name: pkg.name,
@@ -30,6 +31,19 @@ app.get("/info", (_req: Request, res: Response) => {
     license: pkg.license,
   });
 });
+
+// Main routes
+import { urlRoutes } from "./routes/index.routes";
+import { errorHandler } from "./middleware/errorHandler.middleware";
+app.use("/api",urlRoutes);
+
+// Fall back route
+app.use((req : Request, _res : Response, next : NextFunction)=>{
+  next(new UrlError(404, `Route ${req.originalUrl} not found`));
+})
+
+// Global errorHandler
+app.use(errorHandler);
 
 // Server Listen
 listener(app, PORT, pkg);
