17acbb9105f89e6f048a1be441354637079fee85
kaushik <dvlprkaushik@gmail.com>
2025-11-11 18:50:00 +0530

    feature: add Zod validation middleware for URL input and expiry date

---
+++ src/middleware/validation.middleware.ts
@@ -0,0 +1,18 @@
+import { UrlError } from "@/utils/UrlError";
+import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
+
+export const shortenSchema = z.object({
+  url: z.url(),
+  expiresAt: z.string().datetime().optional(),
+});
+
+export const validateShorten = (req : Request, _res : Response, next : NextFunction) => {
+  const result = shortenSchema.safeParse(req.body);
+  if (!result.success){
+    const errors = result.error;
+    next(new UrlError(400,errors.message));
+  }
+  req.body = result.data;
+  next();
+};
